9118b3fc5d4f13087b377ff9ca15797de3e63870
Equipo\Alejandro Isaza <isaza10121@gmail.com>
2018-01-31 21:02:05 -0500

    Set Lives Code Done

---
+++ MTGCounter/Assets/Scripts/SetLives.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetLives : MonoBehaviour
+{ 
+    int score;
+    public Text scoretext1;
+    public Text scoretext2;
+    public Text scoretext3;
+    public Text scoretext4;
+    public Text scoretext5;
+    public Text scoretext6;
+
+    public void TwentyLives()
+    {
+        score = 20;
+        SetCountText();
+    }
+    public void ThirtyLives()
+    {
+        score = 30;
+        SetCountText();
+    }
+    public void FourtyLives()
+    {
+        score = 40;
+        SetCountText();
+    }
+    public void Plus()
+    {
+
+    }
+    public void Minus()
+    {
+
+    }
+    void SetCountText()
+    {
+        scoretext1.text = score.ToString();
+        scoretext2.text = score.ToString();
+        scoretext3.text = score.ToString();
+        scoretext4.text = score.ToString();
+        scoretext5.text = score.ToString();
+        scoretext6.text = score.ToString();
+    }
+}
