59dc622df8ae3206bccb68d28d98457fb6d8b287
Equipo\Alejandro Isaza <isaza10121@gmail.com>
2018-01-31 19:55:59 -0500

    Added Scene Navigation Code

---
+++ MTGCounter/Assets/Scripts/GoToScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+    public void TwoPlayers()
+    {
+        SceneManager.LoadScene("2Player");
+    }
+    public void ThreePlayers()
+    {
+        SceneManager.LoadScene("3Player");
+    }
+    public void FourPlayers()
+    {
+        SceneManager.LoadScene("4Player");
+    }
+    public void FivePlayers()
+    {
+        SceneManager.LoadScene("5Player");
+    }
+    public void SixPlayers()
+    {
+        SceneManager.LoadScene("6Player");
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+
+}
